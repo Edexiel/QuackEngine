f128a6cb291dcb0d3228128fbced578c4e9acbb4
Alex FIGUEIREDO <a.figueiredo@student.isartdigital.com>
2021-02-24 14:35:55 +0100

    framebuffer

---
+++ Editor/src/mainFrameBuffer.cpp
@@ -0,0 +1,53 @@
+#include "glad/gl.h"
+#include "GLFW/glfw3.h"
+#include <cstdio>
+
+int main()
+{
+  GLFWwindow* window;
+
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(1280, 720, "FrameBuffer", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* load Glad OpenGL */
+  int version = gladLoadGL(glfwGetProcAddress);
+//printf("OpenGL version : %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+  if (version == 0)
+  {
+    printf("Failed to initialize OpenGL context");
+    glfwTerminate();
+    return -1;
+  }
+
+  printf("GL_VENDOR = %s\n",   glGetString(GL_VENDOR));
+  printf("GL_RENDERER = %s\n", glGetString(GL_RENDERER));
+  printf("GL_VERSION = %s\n",  glGetString(GL_VERSION));
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
