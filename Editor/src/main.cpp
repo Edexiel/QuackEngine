481783697b8f9d583337fff045b4aac4ae5ebc0a
Guillaume Nisi <guillaume.nisi@gmail.com>
2021-02-26 10:09:46 +0100

    have to commit to switch branch

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -2,23 +2,29 @@
 // Created by g.nisi on 03/02/2021.
 //
 
-
-#include "glad/gl.h"
 #include "GLFW/glfw3.h"
+#include "glad/gl.h"
 
-#include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
 #include "Input/InputManager.hpp"
 #include "Input/PlatformInputGLFW.hpp"
 #include "Resources/ResourcesManager.hpp"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "imgui.h"
 
-#include "Renderer/Texture.hpp"
 #include "Renderer/Model.hpp"
 #include "Renderer/Shader.hpp"
+#include "Renderer/Texture.hpp"
 #include <cstdio>
 
-void debugGLCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+void
+debugGLCallback(GLenum source,
+                GLenum type,
+                GLuint id,
+                GLenum severity,
+                GLsizei length,
+                const GLchar* message,
+                const void* userParam)
 {
   printf("OpenGL error = %s\n", message);
 }
@@ -36,7 +42,8 @@ void debugGLCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLs
   }
 }*/
 
-int main(void)
+int
+main(void)
 {
   GLFWwindow* window;
 
@@ -49,9 +56,8 @@ int main(void)
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
-  if (!window)
-  {
+  window = glfwCreateWindow(1280, 720, "QuackEngine", NULL, NULL);
+  if (!window) {
     glfwTerminate();
     return -1;
   }
@@ -61,130 +67,142 @@ int main(void)
 
   /* load Glad OpenGL */
   int version = gladLoadGL(glfwGetProcAddress);
-//printf("OpenGL version : %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+  // printf("OpenGL version : %d.%d\n", GLAD_VERSION_MAJOR(version),
+  // GLAD_VERSION_MINOR(version));
 
-  if (version == 0)
-  {
+  if (version == 0) {
     printf("Failed to initialize OpenGL context");
     glfwTerminate();
     return -1;
   }
 
-  printf("GL_VENDOR = %s\n",   glGetString(GL_VENDOR));
+  printf("GL_VENDOR = %s\n", glGetString(GL_VENDOR));
   printf("GL_RENDERER = %s\n", glGetString(GL_RENDERER));
-  printf("GL_VERSION = %s\n",  glGetString(GL_VERSION));
+  printf("GL_VERSION = %s\n", glGetString(GL_VERSION));
 
-  //todo
+  // todo
   /*if (GLAD_GL_KHR_debug)
   {
     glEnable(GL_DEBUG_OUTPUT);
     glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
     glDebugMessageCallback(debugGLCallback, nullptr);
-    glDebugMessageControl(GL_DONT_CARE, GL_DEBUG_TYPE_PERFORMANCE, GL_DONT_CARE, 0, nullptr, GL_FALSE);
-    glDebugMessageControl(GL_DONT_CARE, GL_DEBUG_TYPE_OTHER, GL_DONT_CARE, 0, nullptr, GL_FALSE);
+    glDebugMessageControl(GL_DONT_CARE, GL_DEBUG_TYPE_PERFORMANCE, GL_DONT_CARE,
+  0, nullptr, GL_FALSE); glDebugMessageControl(GL_DONT_CARE,
+  GL_DEBUG_TYPE_OTHER, GL_DONT_CARE, 0, nullptr, GL_FALSE);
   }*/
-
-  // Init ImGui
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-  //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
-  //io.ConfigViewportsNoAutoMerge = true;
-  //io.ConfigViewportsNoTaskBarIcon = true;
-
-  //ImGui style
-  ImGui::StyleColorsDark();
-  //ImGui::StyleColorsClassic();
-
-  ImGuiStyle& style = ImGui::GetStyle();
-  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+  // imgui config
   {
-    style.WindowRounding = 0.0f;
-    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-  }
+    // Init ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable
+    // Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
+    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable
+    // Multi-Viewport / Platform Windows io.ConfigViewportsNoAutoMerge = true;
+    // io.ConfigViewportsNoTaskBarIcon = true;
 
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init("#version 460");
+    // ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsClassic();
 
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      style.WindowRounding = 0.0f;
+      style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
 
-  //Windows
-  bool window_scene=true;
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+  } // imguiconfig end
+
+  // Windows
+  bool window_scene = true;
   bool window_viewport = true;
-  bool window_properties=true;
-  bool window_explorer=true;
+  bool window_properties = true;
+  bool window_explorer = true;
   bool window_log = true;
 
-//input manager
+  // todo : doit etre dans le game
+  // input manager
   Input::PlatformInputGLFW platformInput(window);
   Input::InputManager input(platformInput);
 
-//ResourcesManager
-  Resources::ResourcesManager resourcesManager;
+  // wtf ?
+  // todo : doit etre dans la scene et donc dans le game
+  // ResourcesManager
+  //  Resources::ResourcesManager resourcesManager;
+  //
+  //  Renderer::Texture texture =
+  //  resourcesManager.LoadTexture("../../../DirtCube.jpg"); Renderer::Shader
+  //  shader  =
+  //  resourcesManager.LoadShader("../../../BasicVertexShader.vs","../../../BasicFragmentShader.fs");
+  //
+  //  resourcesManager.ReadFiles();
 
-  Renderer::Texture texture = resourcesManager.LoadTexture("../../../DirtCube.jpg");
-  Renderer::Shader  shader  = resourcesManager.LoadShader("../../../BasicVertexShader.vs","../../../BasicFragmentShader.fs");
-
-  resourcesManager.ReadFiles();
-
-
-
-  while (!glfwWindowShouldClose(window))
-  {
+  while (!glfwWindowShouldClose(window)) {
 
     /* Poll for and process events */
     glfwPollEvents();
-    //resourcesManager
+    // resourcesManager
 
-    //imgui
+    // imgui
     {
       ImGui_ImplOpenGL3_NewFrame();
       ImGui_ImplGlfw_NewFrame();
       ImGui::NewFrame();
 
-      //DockSpace
+      // DockSpace
       {
-//        ImGuiWindowFlags window_flags=0;
-//          window_flags |= ImGuiWindowFlags_Popup ;
+        //        ImGuiWindowFlags window_flags=0;
+        //          window_flags |= ImGuiWindowFlags_Popup ;
 
-        //ImGuiID dockspace_id = ImGui::GetID("DockSpace");
-        //ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
+        // ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+        // ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-
-        //Menu bar
+        // Menu bar
         {
-          if(ImGui::BeginMainMenuBar())
-          {
-            if(ImGui::BeginMenu("Files"))
-            {
-              if (ImGui::MenuItem("New scene")) {}
-              if (ImGui::MenuItem("Save scene")) {}
+          if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("Files")) {
+              if (ImGui::MenuItem("New scene")) {
+              }
+              if (ImGui::MenuItem("Save scene")) {
+              }
 
               ImGui::Separator();
-              if (ImGui::MenuItem("Import object")) {}
+              if (ImGui::MenuItem("Import object")) {
+              }
 
               ImGui::EndMenu();
             }
 
-            if (ImGui::BeginMenu("Edit"))
-            {
-              if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
-              if (ImGui::MenuItem("Redo", "CTRL+Y")) {}  // Disabled item
+            if (ImGui::BeginMenu("Edit")) {
+              if (ImGui::MenuItem("Undo", "CTRL+Z")) {
+              }
+              if (ImGui::MenuItem("Redo", "CTRL+Y")) {
+              } // Disabled item
               ImGui::Separator();
-              if (ImGui::MenuItem("Cut", "CTRL+X")) {}
-              if (ImGui::MenuItem("Copy", "CTRL+C")) {}
-              if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+              if (ImGui::MenuItem("Cut", "CTRL+X")) {
+              }
+              if (ImGui::MenuItem("Copy", "CTRL+C")) {
+              }
+              if (ImGui::MenuItem("Paste", "CTRL+V")) {
+              }
               ImGui::EndMenu();
             }
 
-            if(ImGui::BeginMenu("Windows"))
-            {
-              if (ImGui::MenuItem("Viewport",NULL,window_viewport)) {window_viewport=!window_viewport;}
-              if (ImGui::MenuItem("Logs",NULL,window_log)) {window_log=!window_log;}
+            if (ImGui::BeginMenu("Windows")) {
+              if (ImGui::MenuItem("Viewport", NULL, window_viewport)) {
+                window_viewport = !window_viewport;
+              }
+              if (ImGui::MenuItem("Logs", NULL, window_log)) {
+                window_log = !window_log;
+              }
               ImGui::EndMenu();
             }
           }
@@ -192,50 +210,48 @@ int main(void)
         }
       }
 
-      ImGui::ShowDemoWindow();
+      // ImGui::ShowDemoWindow();
 
-      //Scene
+      // Scene
       {
-        ImGui::Begin("Scene",&window_scene);
-        //ImGui::
+        ImGui::Begin("Scene", &window_scene);
+        // ImGui::
         ImGui::End();
       }
 
-
-
-      //ViewPort
-      if(window_viewport){
+      // ViewPort
+      if (window_viewport) {
         ImGui::Begin("Viewport");
-        ImGui::Text("Hello from another window!");
+        // ImGui::Text("Hello from another window!");
+        // ImGui::Image()
         ImGui::End();
       }
 
-      //Properties
-      if(window_properties){
-        ImGui::Begin("Properties",&window_properties);
+      // Properties
+      if (window_properties) {
+        ImGui::Begin("Properties", &window_properties);
         ImGui::End();
       }
 
-      //File Explorer
-      if(window_explorer){
-        ImGui::Begin("Explorer",&window_explorer);
+      // File Explorer
+      if (window_explorer) {
+        ImGui::Begin("Explorer", &window_explorer);
         ImGui::End();
       }
 
-      //Logs
-      if(window_log){
-        ImGui::Begin("Logs",&window_log);
+      // Logs
+      if (window_log) {
+        ImGui::Begin("Logs", &window_log);
         ImGui::End();
       }
       ImGui::Render();
 
       int display_w, display_h;
       glfwGetFramebufferSize(window, &display_w, &display_h);
-      //glViewport(0, 0, display_w, display_h);
-      //glClearColor(1.f,0.f,0.f,0.f);
-      //glClear(GL_COLOR_BUFFER_BIT);
+      // glViewport(0, 0, display_w, display_h);
+      // glClearColor(1.f,0.f,0.f,0.f);
+      // glClear(GL_COLOR_BUFFER_BIT);
       ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
 
       if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
         GLFWwindow* backup_current_context = glfwGetCurrentContext();
