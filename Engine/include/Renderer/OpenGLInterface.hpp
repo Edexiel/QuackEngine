227f523e9eb1b764e0bf7522cc4277ed3c0679b6
Alex FIGUEIREDO <a.figueiredo@student.isartdigital.com>
2021-02-26 13:59:02 +0100

    add function: NewFrame, Delete, SetVertices and SetIndices

--- Engine/include/Renderer/OpenGLInterface.hpp
+++ Engine/include/Renderer/OpenGLInterface.hpp
@@ -1,18 +1,35 @@
 #ifndef QUACKENGINE_OPENGLINTERFACE_HPP
 #define QUACKENGINE_OPENGLINTERFACE_HPP
 
-class GLFWwindow;
+#include "Maths/Matrix4.hpp"
+
 
 namespace Renderer
 {
+struct OpenGLInterfaceImp
+{
+  unsigned int _vao, _vbo, _ebo, _program;
+  Maths::Matrix4 _projectionMatrix, _viewMatrix, _modelMatrix;
+};
 class OpenGLInterface
 {
-public:
-  OpenGLInterface(GLFWwindow* window);
-  ~OpenGLInterface() = default;
+private:
 
-  static GLFWwindow* InitWindow(const char* windowName, const unsigned int& width, const unsigned int& height);
-  void Update(GLFWwindow* window);
+public:
+  OpenGLInterfaceImp* impl;
+  OpenGLInterface();
+  ~OpenGLInterface();
+
+  void SetProjectionMatrix(const Maths::Matrix4& projectionMatrix);
+  void SetViewMatrix(const Maths::Matrix4& viewMatrix);
+  void SetModelMatrix(const Maths::Matrix4& modelMatrix);
+
+  void SetVertices(const float* vertices, const unsigned int& size);
+  void SetIndices(const unsigned int* indices, const unsigned int& size);
+  void NewFrame();
+
+  void Delete();
 };
 }
+
 #endif // QUACKENGINE_OPENGLINTERFACE_HPP
