7c565eda74c196acb6c94587293be72f862c7139
Guillaume Nisi <guillaume.nisi@gmail.com>
2021-03-30 07:11:21 +0000

    Scene #9

--- Engine/include/Scene/Core/EntityManager.hpp
+++ Engine/include/Scene/Core/EntityManager.hpp
@@ -1,5 +1,5 @@
 //
-// Created by lemome on 16/03/2021.
+// Created by gnisi on 16/03/2021.
 //
 
 #ifndef QUACKENGINE_ENTITYMANAGER_HPP
@@ -10,7 +10,6 @@
 #include <bitset>
 #include <algorithm>
 
-#include "Entity.hpp"
 #include "Types.hpp"
 #include "Debug/Assertion.hpp"
 
@@ -18,7 +17,7 @@
 class EntityManager
 {
 private:
-    EntityId _idCount;
+    Entity _idCount = 0;
 
     std::vector<Entity> _entities{};
 public:
@@ -27,26 +26,24 @@ public:
 private:
     std::vector<Signature> _signatures{};
 
-    std::unordered_map<EntityId, size_t> _entityLut{};
+    std::unordered_map<Entity, size_t> _entityLut{};
 
 public:
     EntityManager();
 
-    Entity &Create(std::string &name);
+    Entity Create();
 
-    Entity &Get(EntityId id);
+    Signature GetSignature(Entity id);
 
-    Signature GetSignature(EntityId id);
+    void SetSignature(Entity id, Signature signature);
 
-    void SetSignature(EntityId id, Signature signature);
-
-    void Destroy(EntityId id);
+    void Destroy(Entity id);
 };
 
 /**
  * Entity constructor
  */
- inline EntityManager::EntityManager()
+inline EntityManager::EntityManager()
 {
     _entityLut.reserve(START_SIZE);     //lookup table to translate faster Entity id to index in array
     _entities.reserve(START_SIZE);      //Entity array
@@ -55,39 +52,27 @@ public:
 
 /**
  * Instantiate a new entity and add it to the lookup table
- * @param name Entity's name
  * @return Reference to entity
  */
-inline Entity &EntityManager::Create(std::string &name)
+inline Entity EntityManager::Create()
 {
-    Entity &e = _entities.emplace_back(_idCount++, std::move(name));
+    Entity id = _entities.emplace_back(_idCount++);
     _signatures.emplace_back();
-    _entityLut.insert({e.GetId(), _entities.size() - 1});
+    _entityLut.insert({id, _entities.size() - 1});
 
-    return e;
-}
-
-/**
- * Get a pointer to an Entity by providing an id
- * @param id Entity's id
- * @return A valid pointer to an entity or nullptr if no Entity is associated to id (do not store, lifetime not garanteed)
- */
-inline Entity &EntityManager::Get(EntityId id)
-{
-    Assert_Fatal_Error(_entityLut.find(id) != _entityLut.end(), "Entity id does not exists");
-    return _entities[_entityLut[id]];
+    return id;
 }
 
 /**
  * Removes an Entity by id
  * @param id
  */
-inline void EntityManager::Destroy(EntityId id)
+inline void EntityManager::Destroy(Entity id)
 {
-    Assert_Error(_entityLut.find(id) != _entityLut.end(), "Entity id does not exists");
+    Assert_Error(_entityLut.find(id) == _entityLut.end(), "Entity id does not exists");
 
     size_t index = _entityLut[id]; //index of the entity and signature
-    EntityId backId = _entities.back().GetId();
+    Entity backId = _entities.back();
 
     std::swap(_entities[index], _entities.back());
     _entities.pop_back();
@@ -108,9 +93,9 @@ inline void EntityManager::Destroy(EntityId id)
  * @param id
  * @return The signature of the entity
  */
-inline Signature EntityManager::GetSignature(EntityId id)
+inline Signature EntityManager::GetSignature(Entity id)
 {
-    Assert_Fatal_Error(_entityLut.find(id) != _entityLut.end(), "Entity id does not exists");
+    Assert_Fatal_Error(_entityLut.find(id) == _entityLut.end(), "Entity id does not exists");
 
     return _signatures[_entityLut[id]];
 
@@ -121,9 +106,9 @@ inline Signature EntityManager::GetSignature(EntityId id)
  * @param id
  * @param signature
  */
-inline void EntityManager::SetSignature(EntityId id, Signature signature)
+inline void EntityManager::SetSignature(Entity id, Signature signature)
 {
-    Assert_Error(_entityLut.find(id) != _entityLut.end(), "Entity id does not exists");
+    Assert_Error(_entityLut.find(id) == _entityLut.end(), "Entity id does not exists");
 
     _signatures[_entityLut[id]] = signature;
 }
