1728b0f9cb0232a671ca7435e034df69589a79cd
Alex <a.figueiredo@student.isartdigital.com>
2021-04-01 17:47:41 +0200

    push before merge

--- Editor/src/mainPhysics.cpp
+++ Editor/src/mainPhysics.cpp
@@ -1,9 +1,5 @@
 #include "GLFW/glfw3.h"
-#include "reactphysics3d/reactphysics3d.h"
-
-#include "Scene/System/PhysicsSystem.hpp"
-#include "Scene/Component/RigidBody.hpp"
-#include "Scene/Component/Transform.hpp"
+#include "Maths/Quaternion.hpp"
 
 #include "Renderer/RendererPlatform.hpp"
 #include "Renderer/Shader.hpp"
@@ -11,66 +7,26 @@
 #include "Renderer/Texture.hpp"
 #include "Renderer/Vertex.hpp"
 #include "Renderer/Mesh.hpp"
-#include "Renderer/Light.hpp"
-#include "Renderer/Material.hpp"
-
+#include "Scene/Component/Light.hpp"
 #include "Resources/ResourcesManager.hpp"
+
+#include "Input/PlatformInputGLFW.hpp"
+#include "Input/InputManager.hpp"
+
+#include "Audio/SoundManager.hpp"
+
+
+#include "Scene/Component/Transform.hpp"
+#include "Scene/System/TestSystem.hpp"
 
 
 #include "Scene/Core/World.hpp"
 
-
-#include <cmath>
-
 using namespace Renderer;
-
-const char *vertexShaderFb =
-        {
-                R"GLSL(
-                      #version 330 core
-                      layout (location = 0) in vec3 aPos;
-                      layout (location = 1) in vec3 aNormal;
-                      layout (location = 2) in vec2 aTexCoord;
-
-                      uniform mat4 view;
-
-                      out vec2 TexCoord;
-
-                      void main()
-                      {
-                      gl_Position = view * vec4(aPos, 1.0);
-                      TexCoord = aTexCoord;
-                     }
-                     )GLSL"
-        };
-const char *fragmentShaderFb =
-        {
-                R"GLSL(
-                     #version 330 core
-                     out vec4 FragColor;
-                     in vec2 TexCoord;
-
-                     uniform sampler2D ourTexture;
-
-                     void main()
-                     {
-                      FragColor = texture(ourTexture, TexCoord);
-                     }
-                     )GLSL"
-        };
-
-
-class TestCollision : public rp3d::EventListener
-{
-    virtual void onContact(const CallbackData &callbackData) override
-    {
-        std::cout << "Bonjour\n";
-    }
-};
 
 int main()
 {
-    GLFWwindow *window;
+    GLFWwindow* window    {nullptr};
     unsigned int width = 1280, height = 720;
     /* Initialize the library */
     if (!glfwInit())
@@ -82,35 +38,47 @@ int main()
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(width, height, "RendererPlatform", NULL, NULL);
-    if (!window) {
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    RendererPlatform::LoadGL();
+
     {
-        // loadGL
-        RendererPlatform::LoadGL();
+        World &ecs=World::Instance();
+        ecs.Init();
 
-        Resources::ResourcesManager rm;
+        Entity id = ecs.CreateEntity("Test");
+        Transform t = {Maths::Vector3f::One(), Maths::Vector3f::One(), Maths::Quaternion{}};
+        ecs.AddComponent(id, t);
 
-        // Shader fb
-        Shader shaderFb(Renderer::RendererPlatform::CreateShader(
-                vertexShaderFb, fragmentShaderFb));
-        // Framebuffer
-        Renderer::Framebuffer framebuffer =
-                Renderer::RendererPlatform::CreateFramebuffer(width, height);
+        Entity idRenderTest = ecs.CreateEntity("Test");
+        Transform t2 = {Maths::Vector3f{0,0,10}, Maths::Vector3f::One(), Maths::Quaternion{}};
+        Component::Model md = ecs.GetResourcesManager().LoadModel("../../../eyeball.fbx");
 
-        Mesh quad = RendererPlatform::CreateQuad();
-        Mesh sphere = RendererPlatform::CreateSphere();
+        Material material;
 
-        RendererPlatform::VerticesReading();
+        material.ambient = {1, 1, 1};
+        material.diffuse = {1, 1, 1};
+        material.specular = {1, 1, 1};
+        material.checkLight = true;
+        md.AddMaterial(material);
+        ecs.AddComponent(idRenderTest, t2);
+        ecs.AddComponent(idRenderTest, md);
 
-        Renderer::Light light(Renderer::Light_Type::L_POINT);
+        Entity lightID = ecs.CreateEntity("Light");
+        Entity lightID2 = ecs.CreateEntity("Light");
+        Entity lightID3 = ecs.CreateEntity("Light");
 
-        light.model = Maths::Matrix4::RotateX(-3.1415 / 2) * Maths::Matrix4::Translate({0, 0, 0});
+        Component::Light light;
+
+
+        light.type = Component::Light_Type::L_SPOT;
         light.ambient = {0.0f, 0.1f, 0.0f};
-        light.diffuse = {0.7f, 0.7f, 0.7f};
-        light.specular = {1.0f, 1.0f, 1.0f};
+        light.diffuse = {1,0,0};
+        light.specular = {1, 0, 0};
         light.constant = 1.0f;
         light.linear = 0.0014f;
         light.quadratic = 0.000007f;
@@ -118,97 +86,63 @@ int main()
         light.outerSpotAngle = 10.5;
         light.spotAngle = 8.5;
 
-        ShaderConstructData shd = {1, 1, 0, 0, 0, 0, 0, 0};
 
-        Shader shader = Shader::LoadShader(shd);
+        Transform tl1 = {Maths::Vector3f::One(), Maths::Vector3f::One(), Maths::Quaternion{}};
 
-        RendererPlatform::UseShader(shader.GetID());
-        shader.SetMatrix4
-                (
-                        "projection",
-                        Maths::Matrix4::Perspective(width, height, -1.f, 100.f, 3.1415f / 2.f)
-                );
-        shader.SetMatrix4("view", Maths::Matrix4::Identity());
+        ecs.AddComponent(lightID, light);
+        ecs.AddComponent(lightID, tl1);
 
-        Material material;
-        material.shader = shader;
+        light.type = Component::Light_Type::L_POINT;
+        light.diffuse = {0,1,0};
+        light.specular = {0, 1, 0};
+        Transform tl2 = {Maths::Vector3f::One() * -100, Maths::Vector3f::One(), Maths::Quaternion{}};
 
-        material.ambient = {1, 1, 1};
-        material.diffuse = {1, 1, 1};
-        material.specular = {1, 1, 1};
-        material.shininess = 256;
+        ecs.AddComponent(lightID2, light);
+        ecs.AddComponent(lightID2, tl2);
 
+        light.type = Component::Light_Type::L_DIRECTIONAL;
+        light.diffuse = {0,0,1};
+        light.specular = {0, 0, 1};
+        Transform tl3 = {Maths::Vector3f::Zero(), Maths::Vector3f::One(), Maths::Quaternion{3.1415 / 2, 1, 0, 0}};
+
+
+        ecs.AddComponent(lightID3, light);
+        ecs.AddComponent(lightID3, tl3);
+
+        {
+            Entity CameraEntity = ecs.CreateEntity("Camera");
+
+            Component::Camera camera(1280,
+                                     720,
+                                     1000, -1, 20 * 3.1415/180);
+
+            Transform cameraTrs;
+            ecs.AddComponent(CameraEntity, camera);
+            ecs.AddComponent(CameraEntity, cameraTrs);
+
+            camera.GetFramebuffer().Bind();
+        }
 
         float count = 0;
 
         RendererPlatform::EnableDepthBuffer(true);
 
+        //glfwSetWindowShouldClose(window, 1);
 
-//  Test ecs
-        World &ecs=World::Instance();
-        ecs.Init();
-        ecs.RegisterComponent<Component::RigidBody>();
-        ecs.RegisterComponent<Transform>();
-        auto physicsSystem = ecs.RegisterSystem<PhysicsSystem>();
+        while (!glfwWindowShouldClose(window))
+        {
 
-        Signature signature;
-        signature.set(ecs.GetComponentType<Component::RigidBody>());
-        signature.set(ecs.GetComponentType<Transform>());
-        ecs.SetSystemSignature<PhysicsSystem>(signature);
+            count += 0.0001f;
 
-        Entity id = ecs.CreateEntity("Test");
-
-        Component::RigidBody rbS;
-        ecs.AddComponent(id, rbS);
-        ecs.AddComponent(id, Transform{{0,0,0},{1,1,1},{1,0,0,0}});
-
-        physicsSystem->Init();
-        physicsSystem->AddSphereCollider(id, 1.0f,{0,5,10});
-
-
-
-        while (!glfwWindowShouldClose(window)) {
-
-            physicsSystem->FixedUpdate(1.0f/60.0f);
-
-            count += 0.01f;
-
-            if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
-                glfwSetWindowShouldClose(window, 1);
-            }
             // framebuffer
             {
-                framebuffer.Bind();
-                RendererPlatform::ClearColor({0.0f, 0.5f, 0.5f, 1.f});
-                RendererPlatform::Clear();
-
-                light.model = Maths::Matrix4::Translate({cos(count) * 30, sin(count) * 30, 0});
-
-
-                material.Apply();
-
-                material.shader.SetMatrix4("projection",
-                                           Maths::Matrix4::Perspective(width, height, -1, 10000, 20 * 3.1415 / 180));
-                material.shader.SetMatrix4("view", Maths::Matrix4::Translate({0, -5, 0}) *
-                                                   Maths::Quaternion({1, 0, 0}, -3.14f / 6.f).ToMatrix());
-
-                shader.SetLight(light, 0);
-
-//                material.shader.SetMatrix4("model", Maths::Matrix4::Translate(posSphere) * rotSphere.ToMatrix() *
-//                                                    Maths::Matrix4::Scale({1, 1, 1}));
-
-                material.shader.SetMatrix4("model",Maths::Matrix4::Translate({0,0,10}));
-                sphere.Draw();
-
-                RendererPlatform::BindFramebuffer(0);
+                if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+                {
+                    glfwSetWindowShouldClose(window, 1);
+                }
+                ecs.GetRendererManager().Update();
             }
 
-            RendererPlatform::ClearColor({0.2f, 0.2f, 0.2f, 1.f});
-            RendererPlatform::Clear();
-            shaderFb.Use();
-            shaderFb.SetMatrix4("view", Maths::Matrix4::Identity());
-            framebuffer.BindTexture();
-            quad.Draw();
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
