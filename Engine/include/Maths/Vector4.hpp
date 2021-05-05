b32f70c8675bafeabafcc1dd5d1b5cba83cd9c12
Guillaume Nisi <guillaume.nisi@gmail.com>
2021-05-05 10:00:02 +0200

    vectors

--- Engine/include/Maths/Vector4.hpp
+++ Engine/include/Maths/Vector4.hpp
@@ -6,133 +6,46 @@
 
 namespace Maths
 {
-template<typename T>
-  struct Vector4
-  {
-    union
+    template<typename T>
+    struct Vector4
     {
-      struct
-      {
-        T x, y, z, w;
-      };
+        union
+        {
+            struct
+            {
+                T x, y, z, w;
+            };
 
-      struct
-      {
-        T r, g, b, a;
-      };
-      T e[4];
-      Vector3<T> xyz;
+            struct
+            {
+                T r, g, b, a;
+            };
+            T e[4];
+            Vector3<T> xyz;
+        };
+
+        Vector4();
+        Vector4(T _x, T _y, T _z, T _w);
+        Vector4(Vector3<T> v, T _w);
+        Vector4(T n);
+
+        T Length() const;
+        T SqrLength() const;
+
+        Vector4<T> &Homogenize();
+        Vector3<T> GetHomogenized() const;
+        static void Homogenize(Vector4<T> &v);
+
+        static T DotProduct(const Vector4<T> &v1, const Vector4<T> &v2);
+
+        static Vector4<T> Zero();
+        static Vector4<T> One();
+
+        Vector4<T> operator+(const Vector4<T> &v2) const;
+        Vector4<T> operator-(const Vector4<T> &v2) const;
+        Vector4<T> operator*(const T &f) const;
     };
 
-    Vector4() = default;
-    Vector4(T _x, T _y, T _z, T _w);
-    Vector4(Vector3<T> v, T _w);
-
-    T Length() const;
-    T SqrLength() const;
-
-    Vector4<T>& Homogenize();
-    Vector3<T> GetHomogenized() const;
-    static void Homogenize(Vector4<T>& v);
-
-    static T DotProduct(const Vector4<T>& v1, const Vector4<T>& v2);
-
-    static Vector4<T> Zero();
-    static Vector4<T> One();
-
-    Vector4<T> operator+(const Vector4<T>& v2) const;
-    Vector4<T> operator-(const Vector4<T>& v2) const;
-    Vector4<T> operator*(const T& f) const;
-  };
-
-  typedef Vector4<float> Vector4f;
-  typedef Vector4<float> Color4f;
-  typedef Vector4<double> Vector4d;
-
-template<typename T>
-inline Vector4<T>::Vector4(T _x, T _y, T _z, T _w):x{_x}, y{_y}, z{_z}, w{_w}{}
-
-template<typename T>
-inline Vector4<T>::Vector4(Vector3<T> v, T _w):x{v.x}, y{v.y}, z{v.z}, w{_w}{}
-template<typename T>
-inline T Vector4<T>::Length() const
-{
-    return sqrtf(x * x + y * y + z * z + w * w);
-}
-
-template<typename T>
-inline T Vector4<T>::SqrLength() const
-{
-    return x * x + y * y + z * z + w * w;
-}
-
-template<typename T>
-inline Vector4<T>& Vector4<T>::Homogenize()
-{
-  if (w != 0 && w != 1)
-  {
-    x /= w;
-    y /= w;
-    z /= w;
-  }
-
-  return *this;
-}
-
-template<typename T>
-inline Vector3<T> Vector4<T>::GetHomogenized() const
-{
-    if (w == 0 || w == 1)
-        return xyz;
-
-    return { x / w, y / w, z / w };
-}
-
-template<typename T>
-void Vector4<T>::Homogenize(Vector4<T>& v)
-{
-  if (v.w != 0 && v.w != 1)
-  {
-    v.x /= v.w;
-    v.y /= v.w;
-    v.z /= v.w;
-  }
-}
-
-template<typename T>
-inline T Vector4<T>::DotProduct(const Vector4<T>& v1, const Vector4<T>& v2)
-{
-    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w;
-}
-
-template<typename T>
-inline Vector4<T> Vector4<T>::Zero()
-{
-  return {0, 0, 0, 0};
-}
-
-template<typename T>
-inline Vector4<T> Vector4<T>::One()
-{
-  return {1, 1, 1, 1};
-}
-
-template<typename T>
-inline Vector4<T> Vector4<T>::operator+(const Vector4<T>& v2) const
-{
-  return { this->x + v2.x, this->y + v2.y, this->z + v2.z, this->w + v2.w };
-}
-
-template<typename T>
-inline Vector4<T> Vector4<T>::operator-(const Vector4<T>& v2) const
-{
-  return { this->x - v2.x, this->y - v2.y, this->z - v2.z, this->w - v2.w };
-}
-
-template<typename T>
-inline Vector4<T> Vector4<T>::operator*(const T& f) const
-{
-  return { this->x * f, this->y * f, this->z * f, this->w * f };
-}
+#include "Maths/Vector4.inl"
 }
 #endif // QUACKENGINE_VECTOR4_HPP
