f3ba6ebb897b9df89d5e499d465044d601048c06
Denis Petit <d.petit@student.isartdigital.com>
2021-05-20 10:26:29 +0200

    add comment

--- Engine/include/Renderer/PostProcessManager.hpp
+++ Engine/include/Renderer/PostProcessManager.hpp
@@ -19,11 +19,18 @@ namespace Renderer
     public:
         void Init();
 
-        void AddProcess(ProcessBase* process)
-        {
-            _listProcess.emplace_back(process);
-        }
+        /***
+         * @brief Add a Process to Apply to the screen on Rendering.
+         * Work with inheritance the pointer must be allocated before adding.
+         * It will be destroyed by the manager when needed.
+         * @param process
+         */
+        void AddProcess(ProcessBase* process);
 
+        /***
+         * @brief Apply each Process saved in the manager on the framebuffer
+         * @param framebuffer
+         */
         void ApplyPostProcess(const Framebuffer& framebuffer);
 
         void DrawTextureInFramebuffer(unsigned int framebufferIndex, unsigned int textureIndex);
