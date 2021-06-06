3eee8680e162624806b2ce5c7149c81dbe07e47f
Petit Denis <denis.petit.ritchon@gmail.com>
2021-06-06 14:59:58 +0000

    Fixes

--- Engine/src/Scene/System/ParticleSystem.cpp
+++ Engine/src/Scene/System/ParticleSystem.cpp
@@ -3,12 +3,16 @@
 #include "Engine.hpp"
 #include "Scene/Core/World.hpp"
 
+#include "Renderer/RendererPlatform.hpp"
 #include "Scene/Component/ParticleEmitter.hpp"
 
 using namespace Renderer;
 
 void ParticleSystem::Process(const Framebuffer &buffer, const Mesh &screenMesh)
 {
+    RendererPlatform::EnableDepthBuffer(false);
+    RendererPlatform::SetTransparency(true);
+
     World& world = Engine::Instance().GetCurrentWorld();
     for (Entity entity : _entities)
     {
